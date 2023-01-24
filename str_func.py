1f1a068ae85835318c8f6a6ee780bb1e2619ee22
irinaloskutova <l.irinka@mail.ru>
2023-01-24 23:08:52 +0500

    add def title_word

--- str_func.py
+++ str_func.py
@@ -2,5 +2,10 @@ def upper_word(word):
     """функция возвращает слово/фразу большими буквами"""
     return word.upper()
 
+def title_word(word):
+    """функция возвращает в слове/ во фразе первую букву большой"""
+    return word.title()
+
+
 i = input()
 print(upper_word(i))
