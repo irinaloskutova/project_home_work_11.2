2af163362bfdd9abedc21ee5d882fc9371bdcf67
irinaloskutova <123111550+irinaloskutova@users.noreply.github.com>
2023-01-24 23:41:27 +0500

    добавлена функция маленькие буквы

--- str_func.py
+++ str_func.py
@@ -2,5 +2,10 @@ def upper_word(word):
     """функция возвращает слово/фразу большими буквами"""
     return word.upper()
 
+
+def little_word(word):
+    """функция возвращает слово/фразу маленькими буквами"""
+    return word.lower() 
+
 i = input()
 print(upper_word(i))
